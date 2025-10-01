5354a3350e4d82eb6dad32c4beb9197e5d1c4dfb
Willy <128942050+Willysc10@users.noreply.github.com>
2025-10-01 21:14:14 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/logstashconfig/main.go
@@ -0,0 +1,23 @@
+// cmd/logstashconfig/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "logstashconfig/internal/logstashconfig"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := logstashconfig.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
